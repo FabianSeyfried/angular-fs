f963aacc49d2883c24ef38cd27d0548cf3cc7164
FabianSeyfried <fabian.seyfried@arithnea.de>
2020-01-28 15:03:43 +0100

    3 files modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,6 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import { CartComponent } from './cart/cart.component';
-import { ServiceComponent } from './cart/service/service.component';
 
 
 @NgModule({

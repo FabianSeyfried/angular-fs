823bc4b5d7103441f4837ced58fbfab4f1dc7ed8
FabianSeyfried <fabian.seyfried@arithnea.de>
2020-01-28 15:21:01 +0100

    2 files modified, 1 added, 1 deleted

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import { products } from '../products';
-import { CartService } from '../CartService';
+
+
 
 @Component({
   selector: 'app-product-details',
@@ -13,12 +14,9 @@ product;
 
   constructor(
     private route: ActivatedRoute,
-    private cartService: CartService,
+    
   ) {}
-addToCart(product){
-  window.alert('Your product has been added to the cart!');
-  this.cartService.addToCart(product);
-}
+
   ngOnInit() {
   this.route.paramMap.subscribe(params => {
     this.product = products[+params.get('productId')];

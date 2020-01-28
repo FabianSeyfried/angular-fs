b59197a267316435a2e45a2a3e878acd701aabbe
FabianSeyfried <fabian.seyfried@arithnea.de>
2020-01-28 15:33:33 +0100

    Cart Website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartService } from './cart.service';
+import { CartComponent } from './cart/cart.component';
 
 
 @NgModule({
@@ -18,6 +18,7 @@ import { CartService } from './cart.service';
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent},
+      { path: 'cart', component: CartComponent},
     ])
   ],
   declarations: [
@@ -26,6 +27,7 @@ import { CartService } from './cart.service';
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
+    CartComponent,
   ],
   bootstrap: [ AppComponent ],
   providers: [CartService],

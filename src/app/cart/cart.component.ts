bdcf7717ba44dcc78b290d420c6f27f32e6f7815
FabianSeyfried <fabian.seyfried@arithnea.de>
2020-01-29 08:32:29 +0100

    3 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,21 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
-
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-  
+  items;
+
   constructor(
     private cartService: CartService
   ) { }
 
-items;
-
   ngOnInit() {
     this.items = this.cartService.getItems();
   }
-
 }
